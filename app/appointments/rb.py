8165d30d0ad1a27a1904b0006f2379f8160e3549
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-07-09 23:12:42 +0300

    добавляю постепеноо файлы

--- app/appointments/rb.py
+++ app/appointments/rb.py
@@ -5,7 +5,18 @@ from pydantic import BaseModel, field_serializer
 
 
 class RBAppointmentRead(BaseModel):
-    """Схема ответа для записи на приём (Appointment)."""
+    """Схема ответа для записи на приём (Appointment).
+
+    Attributes:
+        id (int): Уникальный идентификатор записи.
+        patient_id (int): Идентификатор пациента.
+        doctor_id (int): Идентификатор врача.
+        start_time (datetime): Время начала приёма.
+
+    Methods:
+        serialize_start_time(value: datetime, _info: Any) -> str:
+            Форматирует время начала приёма в строку формата 'YYYY-MM-DD HH:MM'.
+    """
 
     id: int  # Уникальный идентификатор записи
     patient_id: int  # ID пациента
@@ -14,7 +25,15 @@ class RBAppointmentRead(BaseModel):
 
     @field_serializer("start_time")
     def serialize_start_time(self, value: datetime, _info: Any) -> str:
-        """Форматирование времени в виде строки: YYYY-MM-DD HH:MM."""
+        """Форматирование времени в виде строки: YYYY-MM-DD HH:MM.
+
+        Args:
+            value (datetime): Входящее значение времени начала приёма.
+            _info (Any): Дополнительная информация сериализатора (не используется).
+
+        Returns:
+            str: Отформатированное время начала приёма.
+        """
         return value.strftime("%Y-%m-%d %H:%M")
 
-    model_config = {"from_attributes": True}  # Важный параметр для ORM объектов в Pydantic 2
+    model_config = {"from_attributes": True}
--- app/appointments/schemas.py
+++ app/appointments/schemas.py
@@ -5,13 +5,16 @@ from pydantic import BaseModel, Field, field_serializer
 
 
 class SAppointmentCreate(BaseModel):
-    """
-    Модель данных для создания записи на приём.
+    """Валидатор данных для создания записи на приём.
 
-    Атрибуты:
+    Attributes:
         doctor_id (int): Идентификатор врача.
         patient_id (int): Идентификатор пациента.
         start_time (datetime): Время начала приёма в формате 'YYYY-MM-DD HH:MM'.
+
+    Methods:
+        serialize_start_time(value: datetime, _info: Any) -> str:
+            Форматирует время начала приёма в строку формата 'YYYY-MM-DD HH:MM'.
     """
 
     doctor_id: int
@@ -26,5 +29,13 @@ class SAppointmentCreate(BaseModel):
 
     @field_serializer("start_time")
     def serialize_start_time(self, value: datetime, _info: Any) -> str:
-        """Форматирование времени в виде строки: YYYY-MM-DD HH:MM."""
+        """Форматирование времени в виде строки: YYYY-MM-DD HH:MM.
+
+        Args:
+            value (datetime): Входящее значение времени начала приёма.
+            _info (Any): Дополнительная информация сериализатора (не используется).
+
+        Returns:
+            str: Отформатированное время начала приёма.
+        """
         return value.strftime("%Y-%m-%d %H:%M")
